5fe38f4f1373eca40f1f7e803372bdb3efa40286
RichardU-Jelly <richardko.risko@gmail.com>
2018-03-29 23:15:00 +0200

    Added creating login token from submited login form

--- CryptoTrack/Controllers/LoginController.cs
+++ CryptoTrack/Controllers/LoginController.cs
@@ -14,9 +14,39 @@ namespace CryptoTrack.Controllers
             return View();
         }
 
+        /*
+            Documentation:
+            This function check whether login form was submited, if yes it generates timestamp and hash from username and
+            create login token. Search database for username and then compare pwd. Create record in DB with token and start
+            of login session. Save login token to server session variable. Return view which is welcome page after login - 
+            view should have different navbar (with logout button instaed).
+        */
         public ActionResult Login() {
+
+            ViewBag.Submitted = false;
+
             if (HttpContext.Request.RequestType == "POST") {
-                
+
+                ViewBag.Submitted = true;
+
+                string name = Request.Form["username"];
+                string pwd = Request.Form["password"];
+
+                string hash = "";
+                try
+                {
+                    hash = Utils.Helpers.MD5Hash(name);
+                }
+                catch(Exception e)
+                {
+                    ViewBag.Message = e.Message;
+                }
+
+                string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+                string token = hash + ":" + timestamp;
+
+                //ViewBag.Message = "Logged in : " + token;
+
             }
             return View();
         }
--- CryptoTrack/Utils/Helpers.cs
+++ CryptoTrack/Utils/Helpers.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace CryptoTrack.Utils
@@ -8,7 +10,18 @@ namespace CryptoTrack.Utils
     public class Helpers
     {
 
-        //public static 
+        //MD5
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
 
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
     }
 }
